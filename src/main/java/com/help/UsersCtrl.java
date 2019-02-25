0e45c0f58c4436acbaf472767b500d0e6c038a1c
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-02-25 14:53:42 -0600

    found and corrected errors with controller

--- src/main/java/com/help/UsersCtrl.java
+++ src/main/java/com/help/UsersCtrl.java
@@ -1,25 +1,24 @@
 package com.help;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+// Controller annotation missing
+@Controller
 public class UsersCtrl {
-
-    @GetMapping("profiles")
+    // GetMapping url did not have the correct path
+    @GetMapping("/profile")
     @ResponseBody
     public String showProfile(){
         return "profile page placeholder";
     }
-
+    //
     @PostMapping("/profile")
-    @ResponseBody
-    public String getProfileInfo(@RequestAttribute String username){
+    @ResponseBody    // @RequestParam annotation is used here with the name of the expected form information
+    public String getProfileInfo(@RequestParam("username") String username){
         System.out.println("username = " + username);
         return "profile page submission placeholder " + username;
     }
@@ -28,13 +27,15 @@ public class UsersCtrl {
     @ResponseBody
     public List<String> showListOfUsers(){
         List<String> users = new ArrayList<>();
-        return users;
+        // Addition to a list must happen before return statement
         users.add("Fer");
         users.add("Ryan");
         users.add("Sophie");
+        return users;
     }
 
     @GetMapping("/forgotPassword")
+    @ResponseBody // For a response @ResponseBody is necessary
     public String forgotPassword(){
         return "forgotPassword page placeholder";
     }
